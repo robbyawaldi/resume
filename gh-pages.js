16e22dd9fae9ec3c30237b1e98c9728c75c42618
robbyawaldi <robbyawaldi@gmail.com>
2022-06-08 12:52:39 +0700

    change to sveltekit

---
+++ gh-pages.js
@@ -0,0 +1,18 @@
+
+import { publish } from 'gh-pages';
+
+publish(
+ 'build', // path to public directory
+ {
+  branch: 'gh-pages',
+  repo: 'https://github.com/robbyawaldi/robbyawaldi.github.io', // Update to point to your repository
+  user: {
+   name: 'robbyawaldi', // update to use your name
+   email: 'robbyawaldi@gmail.com' // Update to use your email
+  },
+  dotfiles: true
+  },
+  () => {
+   console.log('Deploy Complete!');
+  }
+);
---
+++ src/routes/for-ridha.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import gif from "$lib/assets/llove-you.gif"
+</script>
+
+<main class="max-w-2xl w-full h-screen bg-teal-800 mx-auto">
+test
+    <!-- <img src={gif} alt="gif"/> -->
+</main>
