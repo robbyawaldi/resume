5b7eb17b05f682f699c10cd9406dfeb0d31317b7
robbyawaldi <robbyawaldi@gmail.com>
2022-06-08 13:47:43 +0700

    Updates

--- output/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/__layout.svelte.js';
-
-export { module };
-export const index = 0;
-export const entry = 'pages/__layout.svelte-2c3d080a.js';
-export const js = ["pages/__layout.svelte-2c3d080a.js","chunks/index-b6926417.js"];
-export const css = ["assets/pages/__layout.svelte-ced54da7.css"];
