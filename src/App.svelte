ae9a7881da6b74a9ae60400e839cfcc152ccd4a4
robbyawaldi <robbyawaldi@gmail.com>
2022-06-08 15:45:20 +0700

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import Router from 'svelte-spa-router';
 	import Portofolio from './Portofolio.svelte';
-	import Love from './Love.svelte';
 </script>
+
 <Router routes={{
 	'/': Portofolio,
-	'/for-ridha': Love
 }}/>
--- src/Love.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import gif from './assets/llove-you.gif';
-</script>
-
-<img src={gif} alt="gif">
-
-
