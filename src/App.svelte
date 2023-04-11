85c5d5337665d3c1eb86b900953ec2b65030b9d6
Robby Awaldi <robbyawaldi@gmail.com>
2023-04-11 14:50:03 +0700

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,106 @@
 <script lang="ts">
-	import Router from 'svelte-spa-router';
-	import Portofolio from './Portofolio.svelte';
+  import profile from "./assets/profile.jpg";
 </script>
 
-<Router
-	routes={{
-		'/': Portofolio
-	}}
-/>
+<main class="p-4 flex flex-col gap-8 max-w-2xl m-auto">
+  <img
+    class="self-center rounded-full md:w-[200px] w-[150px]"
+    src={profile}
+    alt=""
+    width="500"
+  />
+
+  <div class="">
+    <h1 class="text-3xl font-thin md:text-left text-center leading-3">
+      About <span class="font-semibold text-lg text-blue-400">Me</span>
+    </h1>
+    <p>
+      <span class="block mt-3 font-semibold text-lg"
+        >Hi 👋, I'am Robby Awaldi</span
+      >
+      A passionate Frontend Developer having an experience of building Web with Javascript,
+      React js, Node JS, Docker and some other cool libraries and frameworks.
+    </p>
+  </div>
+
+  <div class="">
+    <h1 class="font-semibold text-lg">Experience</h1>
+    <div class="border-l-2 ml-2">
+      <ul class="ml-3">
+        <li>
+          <h2 class="font-bold">Mitra Erat Technology (2019 - 2021)</h2>
+          <p>
+            Building the CMS application, designing the flow and planning of the
+            Indihome Study learning platform provide services that support
+            business needs, build REST APIs, etc.
+          </p>
+        </li>
+        <li>
+          <h2 class="font-bold">
+            Neural Technologies Indonesia (2021 - Present)
+          </h2>
+          <p>
+            I worked at Neural Technologies Indonesia as a team leader for
+            several years. During my time there, I was responsible for
+            overseeing various projects related to the development of
+            dashboards, ERP platforms, and business intelligence systems. In
+            addition, I developed a reusable library that was utilized for
+            internal purposes, which helped to streamline our development
+            processes.
+          </p>
+          <p>
+            One of my primary responsibilities as a team leader was to ensure
+            that each project was completed efficiently and effectively. To
+            accomplish this, I worked closely with my team to design processes
+            such as flow, CI/CD, and other development practices. I also led the
+            effort to implement these processes throughout the organization,
+            ensuring that they were followed consistently by all team members.
+          </p>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="">
+    <h1 class="font-semibold text-lg">Education</h1>
+    <p>Universitas Indraprasta PGRI / Bachelor of Informatics Engineering</p>
+  </div>
+
+  <div class="">
+    <h1 class="font-semibold text-lg">What Skill I have</h1>
+    <div class="flex gap-2 flex-wrap">
+      <span class="chip">Javascript</span>
+      <span class="chip">CSS</span>
+      <span class="chip">Github</span>
+      <span class="chip">Regex</span>
+      <span class="chip">Docker</span>
+      <span class="chip">Node JS</span>
+      <span class="chip">React JS</span>
+      <span class="chip">Tailwindcss</span>
+    </div>
+  </div>
+</main>
+
+<style global lang="postcss">
+  @import "tailwindcss/base";
+
+  @import "tailwindcss/components";
+
+  @import "tailwindcss/utilities";
+
+  body {
+    @apply bg-gray-50
+				h-full
+				w-full;
+  }
+
+  .chip {
+    @apply bg-gray-200 rounded-3xl p-2;
+  }
+
+  @media (min-width: 768px) {
+    body {
+      @apply h-screen;
+    }
+  }
+</style>
