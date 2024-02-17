06b4e883df0d41e4b7572e9f4778cfca4d33d801
robbyawaldi <robbyawaldi@gmail.com>
2024-02-17 22:00:02 +0700

    Updates

---
+++ assets/index-7a3538cc.js
@@ -0,0 +1,151 @@
+(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const v of document.querySelectorAll('link[rel="modulepreload"]'))_(v);new MutationObserver(v=>{for(const t of v)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&_(a)}).observe(document,{childList:!0,subtree:!0});function l(v){const t={};return v.integrity&&(t.integrity=v.integrity),v.referrerPolicy&&(t.referrerPolicy=v.referrerPolicy),v.crossOrigin==="use-credentials"?t.credentials="include":v.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function _(v){if(v.ep)return;v.ep=!0;const t=l(v);fetch(v.href,t)}})();function p(){}function k(e){return e()}function x(){return Object.create(null)}function n(e){e.forEach(k)}function A(e){return typeof e=="function"}function S(e,s){return e!=e?s==s:e!==s||e&&typeof e=="object"||typeof e=="function"}function j(e){return Object.keys(e).length===0}function B(e,s,l){e.insertBefore(s,l||null)}function L(e){e.parentNode&&e.parentNode.removeChild(e)}function I(e){return document.createElement(e)}function M(e,s,l){l==null?e.removeAttribute(s):e.getAttribute(s)!==l&&e.setAttribute(s,l)}function C(e){return Array.from(e.childNodes)}let h;function z(e){h=e}const d=[],E=[];let o=[];const O=[],T=Promise.resolve();let g=!1;function q(){g||(g=!0,T.then(N))}function u(e){o.push(e)}const w=new Set;let r=0;function N(){if(r!==0)return;const e=h;do{try{for(;r<d.length;){const s=d[r];r++,z(s),F(s.$$)}}catch(s){throw d.length=0,r=0,s}for(z(null),d.length=0,r=0;E.length;)E.pop()();for(let s=0;s<o.length;s+=1){const l=o[s];w.has(l)||(w.add(l),l())}o.length=0}while(d.length);for(;O.length;)O.pop()();g=!1,w.clear(),z(e)}function F(e){if(e.fragment!==null){e.update(),n(e.before_update);const s=e.dirty;e.dirty=[-1],e.fragment&&e.fragment.p(e.ctx,s),e.after_update.forEach(u)}}function H(e){const s=[],l=[];o.forEach(_=>e.indexOf(_)===-1?s.push(_):l.push(_)),l.forEach(_=>_()),o=s}const K=new Set;function D(e,s){e&&e.i&&(K.delete(e),e.i(s))}function G(e,s,l,_){const{fragment:v,after_update:t}=e.$$;v&&v.m(s,l),_||u(()=>{const a=e.$$.on_mount.map(k).filter(A);e.$$.on_destroy?e.$$.on_destroy.push(...a):n(a),e.$$.on_mount=[]}),t.forEach(u)}function J(e,s){const l=e.$$;l.fragment!==null&&(H(l.after_update),n(l.on_destroy),l.fragment&&l.fragment.d(s),l.on_destroy=l.fragment=null,l.ctx=[])}function Q(e,s){e.$$.dirty[0]===-1&&(d.push(e),q(),e.$$.dirty.fill(0)),e.$$.dirty[s/31|0]|=1<<s%31}function R(e,s,l,_,v,t,a,P=[-1]){const c=h;z(e);const i=e.$$={fragment:null,ctx:[],props:t,update:p,not_equal:v,bound:x(),on_mount:[],on_destroy:[],on_disconnect:[],before_update:[],after_update:[],context:new Map(s.context||(c?c.$$.context:[])),callbacks:x(),dirty:P,skip_bound:!1,root:s.target||c.$$.root};a&&a(i.root);let y=!1;if(i.ctx=l?l(e,s.props||{},(f,m,...$)=>{const b=$.length?$[0]:m;return i.ctx&&v(i.ctx[f],i.ctx[f]=b)&&(!i.skip_bound&&i.bound[f]&&i.bound[f](b),y&&Q(e,f)),m}):[],i.update(),y=!0,n(i.before_update),i.fragment=_?_(i.ctx):!1,s.target){if(s.hydrate){const f=C(s.target);i.fragment&&i.fragment.l(f),f.forEach(L)}else i.fragment&&i.fragment.c();s.intro&&D(e.$$.fragment),G(e,s.target,s.anchor,s.customElement),N()}z(c)}class U{$destroy(){J(this,1),this.$destroy=p}$on(s,l){if(!A(l))return p;const _=this.$$.callbacks[s]||(this.$$.callbacks[s]=[]);return _.push(l),()=>{const v=_.indexOf(l);v!==-1&&_.splice(v,1)}}$set(s){this.$$set&&!j(s)&&(this.$$.skip_bound=!0,this.$$set(s),this.$$.skip_bound=!1)}}function V(e){let s;return{c(){s=I("body"),s.innerHTML=`<div class="night svelte-szppvz"></div> 
+  <div id="name" class="text absolute top-1/3 opacity-0 transition-opacity duration-1000 text-center w-full svelte-szppvz">Sabila Az-zahra</div> 
+  <div class="flowers svelte-szppvz"><div class="flower flower--1 svelte-szppvz"><div class="flower__leafs flower__leafs--1 svelte-szppvz"><div class="flower__leaf flower__leaf--1 svelte-szppvz"></div> 
+        <div class="flower__leaf flower__leaf--2 svelte-szppvz"></div> 
+        <div class="flower__leaf flower__leaf--3 svelte-szppvz"></div> 
+        <div class="flower__leaf flower__leaf--4 svelte-szppvz"></div> 
+        <div class="flower__white-circle svelte-szppvz"></div> 
+
+        <div class="flower__light flower__light--1 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--2 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--3 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--4 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--5 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--6 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--7 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--8 svelte-szppvz"></div></div> 
+      <div class="flower__line svelte-szppvz"><div class="flower__line__leaf flower__line__leaf--1 svelte-szppvz"></div> 
+        <div class="flower__line__leaf flower__line__leaf--2 svelte-szppvz"></div> 
+        <div class="flower__line__leaf flower__line__leaf--3 svelte-szppvz"></div> 
+        <div class="flower__line__leaf flower__line__leaf--4 svelte-szppvz"></div> 
+        <div class="flower__line__leaf flower__line__leaf--5 svelte-szppvz"></div> 
+        <div class="flower__line__leaf flower__line__leaf--6 svelte-szppvz"></div></div></div> 
+
+    <div class="flower flower--2 svelte-szppvz"><div class="flower__leafs flower__leafs--2 svelte-szppvz"><div class="flower__leaf flower__leaf--1 svelte-szppvz"></div> 
+        <div class="flower__leaf flower__leaf--2 svelte-szppvz"></div> 
+        <div class="flower__leaf flower__leaf--3 svelte-szppvz"></div> 
+        <div class="flower__leaf flower__leaf--4 svelte-szppvz"></div> 
+        <div class="flower__white-circle svelte-szppvz"></div> 
+
+        <div class="flower__light flower__light--1 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--2 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--3 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--4 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--5 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--6 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--7 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--8 svelte-szppvz"></div></div> 
+      <div class="flower__line svelte-szppvz"><div class="flower__line__leaf flower__line__leaf--1 svelte-szppvz"></div> 
+        <div class="flower__line__leaf flower__line__leaf--2 svelte-szppvz"></div> 
+        <div class="flower__line__leaf flower__line__leaf--3 svelte-szppvz"></div> 
+        <div class="flower__line__leaf flower__line__leaf--4 svelte-szppvz"></div></div></div> 
+
+    <div class="flower flower--3 svelte-szppvz"><div class="flower__leafs flower__leafs--3 svelte-szppvz"><div class="flower__leaf flower__leaf--1 svelte-szppvz"></div> 
+        <div class="flower__leaf flower__leaf--2 svelte-szppvz"></div> 
+        <div class="flower__leaf flower__leaf--3 svelte-szppvz"></div> 
+        <div class="flower__leaf flower__leaf--4 svelte-szppvz"></div> 
+        <div class="flower__white-circle svelte-szppvz"></div> 
+
+        <div class="flower__light flower__light--1 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--2 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--3 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--4 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--5 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--6 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--7 svelte-szppvz"></div> 
+        <div class="flower__light flower__light--8 svelte-szppvz"></div></div> 
+      <div class="flower__line svelte-szppvz"><div class="flower__line__leaf flower__line__leaf--1 svelte-szppvz"></div> 
+        <div class="flower__line__leaf flower__line__leaf--2 svelte-szppvz"></div> 
+        <div class="flower__line__leaf flower__line__leaf--3 svelte-szppvz"></div> 
+        <div class="flower__line__leaf flower__line__leaf--4 svelte-szppvz"></div></div></div> 
+
+    <div class="grow-ans svelte-szppvz" style="--d:1.2s"><div class="flower__g-long svelte-szppvz"><div class="flower__g-long__top svelte-szppvz"></div> 
+        <div class="flower__g-long__bottom svelte-szppvz"></div></div></div> 
+
+    <div class="growing-grass svelte-szppvz"><div class="flower__grass flower__grass--1 svelte-szppvz"><div class="flower__grass--top svelte-szppvz"></div> 
+        <div class="flower__grass--bottom svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--1 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--2 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--3 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--4 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--5 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--6 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--7 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--8 svelte-szppvz"></div> 
+        <div class="flower__grass__overlay svelte-szppvz"></div></div></div> 
+
+    <div class="growing-grass svelte-szppvz"><div class="flower__grass flower__grass--2 svelte-szppvz"><div class="flower__grass--top svelte-szppvz"></div> 
+        <div class="flower__grass--bottom svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--1 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--2 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--3 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--4 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--5 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--6 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--7 svelte-szppvz"></div> 
+        <div class="flower__grass__leaf flower__grass__leaf--8 svelte-szppvz"></div> 
+        <div class="flower__grass__overlay svelte-szppvz"></div></div></div> 
+
+    <div class="grow-ans svelte-szppvz" style="--d:2.4s"><div class="flower__g-right flower__g-right--1 svelte-szppvz"><div class="leaf svelte-szppvz"></div></div></div> 
+
+    <div class="grow-ans svelte-szppvz" style="--d:2.8s"><div class="flower__g-right flower__g-right--2 svelte-szppvz"><div class="leaf svelte-szppvz"></div></div></div> 
+
+    <div class="grow-ans svelte-szppvz" style="--d:2.8s"><div class="flower__g-front svelte-szppvz"><div class="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--1 svelte-szppvz"><div class="flower__g-front__leaf svelte-szppvz"></div></div> 
+        <div class="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--2 svelte-szppvz"><div class="flower__g-front__leaf svelte-szppvz"></div></div> 
+        <div class="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--3 svelte-szppvz"><div class="flower__g-front__leaf svelte-szppvz"></div></div> 
+        <div class="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--4 svelte-szppvz"><div class="flower__g-front__leaf svelte-szppvz"></div></div> 
+        <div class="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--5 svelte-szppvz"><div class="flower__g-front__leaf svelte-szppvz"></div></div> 
+        <div class="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--6 svelte-szppvz"><div class="flower__g-front__leaf svelte-szppvz"></div></div> 
+        <div class="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--7 svelte-szppvz"><div class="flower__g-front__leaf svelte-szppvz"></div></div> 
+        <div class="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--8 svelte-szppvz"><div class="flower__g-front__leaf svelte-szppvz"></div></div> 
+        <div class="flower__g-front__line svelte-szppvz"></div></div></div> 
+
+    <div class="grow-ans svelte-szppvz" style="--d:3.2s"><div class="flower__g-fr svelte-szppvz"><div class="leaf svelte-szppvz"></div> 
+        <div class="flower__g-fr__leaf flower__g-fr__leaf--1 svelte-szppvz"></div> 
+        <div class="flower__g-fr__leaf flower__g-fr__leaf--2 svelte-szppvz"></div> 
+        <div class="flower__g-fr__leaf flower__g-fr__leaf--3 svelte-szppvz"></div> 
+        <div class="flower__g-fr__leaf flower__g-fr__leaf--4 svelte-szppvz"></div> 
+        <div class="flower__g-fr__leaf flower__g-fr__leaf--5 svelte-szppvz"></div> 
+        <div class="flower__g-fr__leaf flower__g-fr__leaf--6 svelte-szppvz"></div> 
+        <div class="flower__g-fr__leaf flower__g-fr__leaf--7 svelte-szppvz"></div> 
+        <div class="flower__g-fr__leaf flower__g-fr__leaf--8 svelte-szppvz"></div></div></div> 
+
+    <div class="long-g long-g--0 svelte-szppvz"><div class="grow-ans svelte-szppvz" style="--d:3s"><div class="leaf leaf--0 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:2.2s"><div class="leaf leaf--1 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:3.4s"><div class="leaf leaf--2 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:3.6s"><div class="leaf leaf--3 svelte-szppvz"></div></div></div> 
+
+    <div class="long-g long-g--1 svelte-szppvz"><div class="grow-ans svelte-szppvz" style="--d:3.6s"><div class="leaf leaf--0 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:3.8s"><div class="leaf leaf--1 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:4s"><div class="leaf leaf--2 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:4.2s"><div class="leaf leaf--3 svelte-szppvz"></div></div></div> 
+
+    <div class="long-g long-g--2 svelte-szppvz"><div class="grow-ans svelte-szppvz" style="--d:4s"><div class="leaf leaf--0 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:4.2s"><div class="leaf leaf--1 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:4.4s"><div class="leaf leaf--2 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:4.6s"><div class="leaf leaf--3 svelte-szppvz"></div></div></div> 
+
+    <div class="long-g long-g--3 svelte-szppvz"><div class="grow-ans svelte-szppvz" style="--d:4s"><div class="leaf leaf--0 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:4.2s"><div class="leaf leaf--1 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:3s"><div class="leaf leaf--2 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:3.6s"><div class="leaf leaf--3 svelte-szppvz"></div></div></div> 
+
+    <div class="long-g long-g--4 svelte-szppvz"><div class="grow-ans svelte-szppvz" style="--d:4s"><div class="leaf leaf--0 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:4.2s"><div class="leaf leaf--1 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:3s"><div class="leaf leaf--2 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:3.6s"><div class="leaf leaf--3 svelte-szppvz"></div></div></div> 
+
+    <div class="long-g long-g--5 svelte-szppvz"><div class="grow-ans svelte-szppvz" style="--d:4s"><div class="leaf leaf--0 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:4.2s"><div class="leaf leaf--1 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:3s"><div class="leaf leaf--2 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:3.6s"><div class="leaf leaf--3 svelte-szppvz"></div></div></div> 
+
+    <div class="long-g long-g--6 svelte-szppvz"><div class="grow-ans svelte-szppvz" style="--d:4.2s"><div class="leaf leaf--0 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:4.4s"><div class="leaf leaf--1 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:4.6s"><div class="leaf leaf--2 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:4.8s"><div class="leaf leaf--3 svelte-szppvz"></div></div></div> 
+
+    <div class="long-g long-g--7 svelte-szppvz"><div class="grow-ans svelte-szppvz" style="--d:3s"><div class="leaf leaf--0 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:3.2s"><div class="leaf leaf--1 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:3.5s"><div class="leaf leaf--2 svelte-szppvz"></div></div> 
+      <div class="grow-ans svelte-szppvz" style="--d:3.6s"><div class="leaf leaf--3 svelte-szppvz"></div></div></div></div>`,M(s,"class","svelte-szppvz")},m(l,_){B(l,s,_)},p,i:p,o:p,d(l){l&&L(s)}}}function W(e){return setTimeout(()=>{document.getElementById("name").classList.add("opacity-100")},3e3),[]}class X extends U{constructor(s){super(),R(this,s,W,V,S,{})}}new X({target:document.getElementById("app")});
